d9f8bed234699e380cdca80f3fc7326283a30591
peacemeeper <peacemeeper@gmail.com>
2025-09-24 12:32:14 +0300

    Update main.cpp

--- semester_1/lab3_pointers_and_arrays/main.cpp
+++ semester_1/lab3_pointers_and_arrays/main.cpp
@@ -1,44 +1,42 @@
-
 #include <iostream>
 
-int main()
-{   setlocale(LC_ALL, "Russian");
-    int a,b,n,max,sum,numberOfNegativeNumbers;
-    a=0;
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int a, b, n, max, sum, numberOfNegativeNumbers;
+    a = 0;
     max = 0;
     sum = 0;
-    std::cout<<"Введите количество элементов в массиве (Натуральное число)"<<std::endl;
-    if(!(std::cin>>n)){
-        std::cout<<"Введите корректное значение"<<std::endl;
+    std::cout << "Введите количество элементов в массиве (Натуральное число)"<< std::endl;
+    if (!(std::cin >> n)) {
+        std::cout << "Введите корректное значение" << std::endl;
         return 0;
     }
     int array[n];
     while (a < n) {
-        std::cout<<"Введите "<< (a+1)<< " элемент массива"<<std::endl;
-        std::cin>>array[a];
-        if ( array[a]*array[a] > array[a-1]*array[a-1] ) {
+        std::cout << "Введите " << (a + 1) << " элемент массива" << std::endl;
+        std::cin >> array[a];
+        if (array[a] * array[a] > array[a - 1] * array[a - 1]) {
             max++;
         }
         a++;
-    } 
+    }
     a = 0;
-    if (array[a] > 0){
-        while ( a < n) {
+    if (array[a] > 0) {
+        while (a < n) {
             sum = sum + array[a];
             a++;
         }
-        
-    }
-    else if ( array[a]<0) {
-        while (array[a]<0){
+
+    } else if (array[a] < 0) {
+        while (array[a] < 0) {
             a++;
         }
-        while (a<n){
+        while (a < n) {
             sum = sum + array[a];
             a++;
         }
     }
-    std::cout<<"Сумма элементов массива,расположенных после первого положительного числа = "<<sum<<std::endl;
-    std::cout<<"Номер максимального по модулю элемента массива = "<< max <<std::endl;
+    std::cout << "Сумма элементов массива,расположенных после первого положительного числа = "<< sum << std::endl;
+    std::cout << "Номер максимального по модулю элемента массива = " << max << std::endl;
     return 0;
 }
