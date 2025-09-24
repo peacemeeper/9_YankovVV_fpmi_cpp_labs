8e1f1a4654f86ed56fe139840cc2d8a21c4536dc
peacemeeper <peacemeeper@gmail.com>
2025-09-24 11:54:17 +0300

    Add files via upload

---
+++ semester_1/lab3_pointers_and_arrays/main.cpp
@@ -0,0 +1,46 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+int main()
+{   int a,b,n,max,sum,numberOfNegativeNumbers;
+    a=0;
+    max = 0;
+    sum = 0;
+    std::cout<<"Введите количество элементов в массиве (Натуральное число)"<<std::endl;
+    std::cin>>n;
+    int array[n];
+    while (a < n) {
+        std::cout<<"Введите "<< (a+1)<< " элемент массива"<<std::endl;
+        std::cin>>array[a];
+        if ( array[a] > max ) {
+            max = a ;
+        }
+        a++;
+    } 
+    a = 0;
+    if (array[a] > 0){
+        while ( a < n) {
+            sum = sum + array[a];
+            a++;
+        }
+        
+    }
+    else if ( array[a]<0) {
+        while (array[a]<0){
+            a++;
+        }
+        while (a<n){
+            sum = sum + array[a];
+            a++;
+        }
+    }
+    std::cout<<sum<<std::endl;
+    return 0;
+}
