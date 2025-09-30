fcdefb863e6a9a0319ef81806ebac9d7a74e5616
peacemeeper <peacemeeper@gmail.com>
2025-09-30 23:34:43 +0300

    Update main.cpp

--- semester_1/lab3_pointers_and_arrays/main.cpp
+++ semester_1/lab3_pointers_and_arrays/main.cpp
@@ -1,42 +1,42 @@
+
 #include <iostream>
 
-int main() {
-    setlocale(LC_ALL, "Russian");
-    int a, b, n, max, sum, numberOfNegativeNumbers;
-    a = 0;
-    max = 0;
-    sum = 0;
-    std::cout << "Введите количество элементов в массиве (Натуральное число)"<< std::endl;
-    if (!(std::cin >> n)) {
-        std::cout << "Введите корректное значение" << std::endl;
-        return 0;
-    }
-    int array[n];
+int main()
+{
+    int a,a1,n,maxLength, length;
+    maxLength = 1;
+    a1=0;
+    a=0;
+    length=1;
+    std::cout<<"Введите количество элементов в массиве (Целое натуральное число)"<<std::endl;
+    std::cin>>n;
+    int *array = new int[n];
     while (a < n) {
         std::cout << "Введите " << (a + 1) << " элемент массива" << std::endl;
-        std::cin >> array[a];
-        if (array[a] * array[a] > array[a - 1] * array[a - 1]) {
-            max++;
-        }
+        std::cin>>array[a];
         a++;
     }
-    a = 0;
-    if (array[a] > 0) {
-        while (a < n) {
-            sum = sum + array[a];
-            a++;
-        }
-
-    } else if (array[a] < 0) {
-        while (array[a] < 0) {
-            a++;
-        }
-        while (a < n) {
-            sum = sum + array[a];
-            a++;
-        }
-    }
-    std::cout << "Сумма элементов массива,расположенных после первого положительного числа = "<< sum << std::endl;
-    std::cout << "Номер максимального по модулю элемента массива = " << max << std::endl;
+    a=0;
+    while (a < n){
+            a1=a+1;
+            length=1;
+                while ( a1 < n){
+                    if (array[a] != array[a1]){
+                        while ( array[a] != array[a1]){
+                    }
+                length++;
+                a1++;
+                if ( length > maxLength ) {
+                    maxLength= length;
+                }
+             }
+             next:
+             a++;
+            }
+    std::cout<<maxLength<<std::endl;
+    std::cout<<a<<std::endl;
+    std::cout<<a1<<std::endl;
     return 0;
 }
+}
+
