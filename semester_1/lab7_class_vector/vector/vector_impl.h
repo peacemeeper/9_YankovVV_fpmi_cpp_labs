8f95ad85e91576207aa5de742a3bfecbfc890ca5
peacemeeper <peacemeeper@gmail.com>
2025-12-16 20:50:41 +0300

    Update vector_impl.h

--- semester_1/lab7_class_vector/vector/vector_impl.h
+++ semester_1/lab7_class_vector/vector/vector_impl.h
@@ -1,5 +1,27 @@
 #pragma once
+#include <iostream> 
+#include <initializator_list>
 
 class Vector {
-    // your code goes here
+int* arr;
+size_t size_ = 0;
+size_t capacity_ = 0;
+public:
+int& operator[](size_t index);
+const int& operator[](size_t index);
+Vector();
+Vector(size_t size);
+Vector(std::initializer_list<int> list);
+~Vector();
+void swap(Vector& another);
+int At(size_t index);
+const int At(size_t index) const;
+size_t Size() const;
+size_t Capacity();
+void  Reserve(size_t newCapacity_);
+void PushBack(int value);
+void PopBack();
+void Clear();
+friend std::ostream& operator<<(std::ostream& output, const Vector& v);
 };
+
